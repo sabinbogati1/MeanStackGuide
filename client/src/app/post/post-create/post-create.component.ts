7de42346c8b2c0a17b1592aec24e05f5dd8c0b20
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-05-07 06:54:49 +0545

    Added Angular material

---
+++ client/src/app/post/post-create/post-create.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  newPost = "";
+  enteredValue = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddPost(postInput: HTMLTextAreaElement){
+    this.newPost = this.enteredValue;
+  }
+
+}
