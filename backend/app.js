941fb79b2f086df39a985093bd8d739bd420462b
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-05-09 06:29:29 +0545

    Fetch Initial Posts..

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log("First Middleware");
-    next();
-});
+app.use("/api/posts", (req,res,next) =>{
+    const posts = [
+        {id: "1", title: "First Server Side post", content: "This is coming from server"},
+        {id: "2", title: "Second Server Side post", content: "This is coming from server"}
+    ];
+    res.status(200).json({
+        message: "Posts Fetched Successfully",
+        posts: posts
+    });
+})
 
-app.use((req, res,next)  =>{
-    res.send("Hello From Express");
-});
+// app.use((req,res,next) =>{
+//     console.log("First Middleware");
+//     next();
+// });
+
+// app.use((req, res,next)  =>{
+//     res.send("Hello From Express");
+// });
 
 module.exports = app;
